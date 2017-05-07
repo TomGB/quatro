a501884e9988771018534a0ea045d94f06b37c72
TomGB <tgbanister@gmail.com>
2017-05-08 00:45:54 +0100

    basic AI done

--- main.js
+++ main.js
@@ -32,8 +32,8 @@ function createBoard() {
 var game = {
   board: [],
   turn: 0,
-  playerOne: "Tom",
-  playerTwo: "Tim",
+  playerOne: "Rob",
+  playerTwo: "AI",
   pieceSelected: null,
   playerWon: false,
   allPieces: [],
@@ -73,6 +73,13 @@ function playTurn(e) {
       }
 
       draw(game);
+
+      if (game.turn % 2 && game.useAI) {
+        AIPlacePiece();
+        if (!game.playerWon) {
+          AISelectPiece();
+        }
+      }
     } else {
       for (var i = 0; i < 4; i++) {
         for (var j = 0; j < 4; j++) {
@@ -90,10 +97,6 @@ function playTurn(e) {
             game.playerWon = checkWin(game.board);
 
             draw(game);
-
-            if (game.useAI) {
-              makeAIMove();
-            }
           }
         }
       }
@@ -101,18 +104,74 @@ function playTurn(e) {
   }
 }
 
-function makeAIMove() {
+function AISelectPiece() {
+  console.log('AI select piece');
   var tempBoard = JSON.parse(JSON.stringify(game.board));
 
+  for (var p = 0; p < game.allPieces.length; p++) {
+    var win = 0;
+    for (var i = 0; i < tempBoard.length; i++) {
+      for (var j = 0; j < tempBoard[i].length; j++) {
+        if (tempBoard[i][j] == null) {
+          tempBoard[i][j] = game.allPieces[p];
+          win += checkWin(tempBoard);
+        }
+        tempBoard = JSON.parse(JSON.stringify(game.board));
+      }
+    }
+
+    if (win == 0) {
+      game.pieceSelected = game.allPieces[p];
+      console.log('selected piece:', game.allPieces[p].num);
+      game.turn ++;
+      game.allPieces.splice(p, 1);
+      draw(game);
+      return;
+    }
+  }
+
+  var pieceNum = Math.floor(Math.random() * game.allPieces.length);
+
+  console.log('randomly selecting piece num:', pieceNum);
+
+  game.pieceSelected = game.allPieces[pieceNum];
+  console.log('selected piece:', game.allPieces[pieceNum].num);
+  game.turn ++;
+  game.allPieces.splice(pieceNum, 1);
+  draw(game);
+  return;
+}
+
+function AIPlacePiece() {
+  console.log('ai plece piece');
+  var tempBoard = JSON.parse(JSON.stringify(game.board));
   for (var i = 0; i < tempBoard.length; i++) {
     for (var j = 0; j < tempBoard[i].length; j++) {
       if (tempBoard[i][j] == null) {
-        for (var p = 0; p < game.allPieces.length; p++) {
-          tempBoard[i][j] = game.allPieces[p];
-          console.log(checkWin(tempBoard));
+        tempBoard[i][j] = game.pieceSelected;
+        if (checkWin(tempBoard)) {
+          game.board[i][j] = game.pieceSelected;
+          console.log(game.board);
+          game.pieceSelected = null;
+          game.playerWon = checkWin(game.board);
+          draw(game);
+          return;
         }
       }
       tempBoard = JSON.parse(JSON.stringify(game.board));
+    }
+  }
+
+  while (true) {
+    var x = Math.floor(Math.random() * 4);
+    var y = Math.floor(Math.random() * 4);
+    console.log('random:', x, y);
+    if (tempBoard[x][y] == null) {
+      game.board[x][y] = game.pieceSelected;
+      console.log(game.board);
+      game.pieceSelected = null;
+      draw(game);
+      return;
     }
   }
 }
